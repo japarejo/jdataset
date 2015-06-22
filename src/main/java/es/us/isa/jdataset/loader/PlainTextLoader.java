56dd28c6817054584ce32aef4a4283c64ea37f66
japarejo <japarejo@us.es>
2015-06-22 13:44:02 +0200

    Improved plain text loading

--- src/main/java/es/us/isa/jdataset/loader/PlainTextLoader.java
+++ src/main/java/es/us/isa/jdataset/loader/PlainTextLoader.java
@@ -7,6 +7,7 @@ package es.us.isa.jdataset.loader;
 
 import com.google.common.base.Function;
 import com.google.common.collect.Sets;
+import com.google.common.primitives.Doubles;
 import com.google.common.primitives.Ints;
 import es.us.isa.jdataset.Column;
 import es.us.isa.jdataset.DataSet;
@@ -14,10 +15,14 @@ import es.us.isa.jdataset.SimpleDataSet;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -30,7 +35,8 @@ public class PlainTextLoader<X> extends AbstractFileTypeLoader{
     public static final Set<String> SUPPORTED_EXTENSIONS=Sets.newHashSet("txt");
     private String rowSeparator;
     private String columnSeparator;    
-    Map<Integer,Function<String,?>> columnTypeConversors;
+    Map<String,Function<String,?>> columnTypeConversors;
+    Map<Class<?>,Function<String,?>> perTypeConversors;
     Function<String,X> defaultConversor;            
     Boolean hasColumnHeaders;
     
@@ -39,17 +45,20 @@ public class PlainTextLoader<X> extends AbstractFileTypeLoader{
 
     public PlainTextLoader(String rowSeparator, String columnSeparator,Boolean hasColumnHeaders)
     {
-        this(rowSeparator,columnSeparator,hasColumnHeaders, new HashMap<Integer, Function<String, ?>>(),null);
+        this(rowSeparator,columnSeparator,hasColumnHeaders, new HashMap<String, Function<String, ?>>(),null);
+        this.defaultConversor=(Function<String,X>)generateDefaultValueConversor(String.class);
     }
             
     
     
-    public PlainTextLoader(String rowSeparator, String columnSeparator,Boolean hasColumnHeaders, Map<Integer, Function<String, ?>> columnTypeConversors, Function<String, X> defaultConversor) {
+    public PlainTextLoader(String rowSeparator, String columnSeparator,Boolean hasColumnHeaders, Map<String, Function<String, ?>> columnTypeConversors, Function<String, X> defaultConversor) {
         this.rowSeparator = rowSeparator;
         this.columnSeparator = columnSeparator;
-        this.columnTypeConversors = columnTypeConversors;        
-        this.defaultConversor = defaultConversor;        
+        this.columnTypeConversors=columnTypeConversors;
+        this.perTypeConversors = new HashMap<>();                
         this.hasColumnHeaders = hasColumnHeaders;    
+        this.defaultConversor = defaultConversor;
+        generateTypeConversors();
     }
     
     private void createColumns(DataSet dataset,String[] firstRow,String[] secondRow) {
@@ -65,13 +74,13 @@ public class PlainTextLoader<X> extends AbstractFileTypeLoader{
 
     private Class<?> extractColumnClass(String header,String value) {
         Class<?> result=null;
-        Function<String,?> function=columnTypeConversors.get(headersIndexes.get(header));
+        /*Function<String,?> function=columnTypeConversors.get(headersIndexes.get(header));
         if(function==null)
             function=defaultConversor;
         if(function!=null){
-            result=(Class<?>) ((ParameterizedType) getClass()
+            result=(Class<?>) ((ParameterizedType) function.getClass()
                             .getGenericSuperclass()).getActualTypeArguments()[1];
-        }else
+        }else*/
             result=inferColumnClass(header,value);
         return result;
     }
@@ -83,9 +92,9 @@ public class PlainTextLoader<X> extends AbstractFileTypeLoader{
     private Class<?> inferClassFromValue(String value)
     {
         Class<?> result=null;
-        if(Ints.tryParse(value)==null)
+        if(Ints.tryParse(value)!=null)
             result=Integer.class;
-        else if(StringUtils.isNumeric(value))
+        else if(Doubles.tryParse(value)!=null || Doubles.tryParse(value.replace(",", "."))!=null)
             result=Double.class;
         else if(value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false"))
             result=Boolean.class;
@@ -108,8 +117,9 @@ public class PlainTextLoader<X> extends AbstractFileTypeLoader{
         {
             for(int i=0;i<firstRow.length;i++)
             {
-                result[i]=DEFAULT_VAR_NAME_PREFIX+i;
+                result[i]=DEFAULT_VAR_NAME_PREFIX+i;                
             }
+            this.hasColumnHeaders=Boolean.FALSE;
         }else{
             result=firstRow;
             this.hasColumnHeaders=Boolean.TRUE;
@@ -124,10 +134,11 @@ public class PlainTextLoader<X> extends AbstractFileTypeLoader{
         for(int j=0;j<data.length;j++)
         {
             column=dataset.getColumn(headers[j].trim());
-            valueTypeConversor=columnTypeConversors.get(j);            
-            if(valueTypeConversor==null){                
+            valueTypeConversor=columnTypeConversors.get(column.getName());
+            if(valueTypeConversor==null)
+                valueTypeConversor=perTypeConversors.get(column.getType());            
+            if(valueTypeConversor==null)                
                 valueTypeConversor=defaultConversor;
-            }
             if(valueTypeConversor!=null)
                 column.set(i, valueTypeConversor.apply(data[j]));
             else
@@ -163,6 +174,70 @@ public class PlainTextLoader<X> extends AbstractFileTypeLoader{
         return SUPPORTED_EXTENSIONS;
     }        
     
-    
+    private static <X> Function<String,X> generateDefaultValueConversor(final Class<X> c)
+    {
+        
+        return new Function<String, X>( ) {
+            private final Class imageClass=c;
+            
+            @Override
+            public X apply(String value) {
+                X result=null;
+                if(Integer.class.equals(imageClass)){
+                    Integer intVal=Integer.parseInt(value);
+                    result=(X)intVal;
+                }else if(Double.class.equals(imageClass)){
+                    Double doubleVal=Double.parseDouble(value);
+                    result=(X)doubleVal;
+                }else if(Boolean.class.equals(imageClass)){
+                    Boolean booleanValue=Boolean.parseBoolean(value);
+                    result=(X)booleanValue;
+                }else if(imageClass.isEnum()){
+                    try {
+                        Method m=(imageClass.getMethod("", String.class));
+                        result=(X)m.invoke(null, value);
+                    } catch (NoSuchMethodException ex) {
+                        Logger.getLogger(PlainTextLoader.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (SecurityException ex) {
+                        Logger.getLogger(PlainTextLoader.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IllegalAccessException ex) {
+                        Logger.getLogger(PlainTextLoader.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IllegalArgumentException ex) {
+                        Logger.getLogger(PlainTextLoader.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (InvocationTargetException ex) {
+                        Logger.getLogger(PlainTextLoader.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }else if(String.class.equals(imageClass)){
+                    result=(X)value;
+                }
+                return result;
+            }
+        };
+    }
+
+    private void generateTypeConversors() {
+        Function<String,Integer> integerConversor=new Function<String,Integer>() {
+            @Override public Integer apply(String f) {
+                return Integer.valueOf(f);
+            }
+        };
+        perTypeConversors.put(Integer.class, integerConversor);
+        Function<String,Double> doubleConversor=new Function<String,Double>() {
+            @Override  public Double apply(String f) {
+                Double result=null;
+                result=Doubles.tryParse(f);
+                if(result==null)
+                    result=Double.parseDouble(f.replace(",", "."));
+                return result;
+            }
+        };
+        perTypeConversors.put(Double.class, doubleConversor);
+        Function<String,Boolean> booleanConversor=new Function<String, Boolean>() {
+            @Override public Boolean apply(String f) {
+                return Boolean.valueOf(f);
+            }
+        };
+        perTypeConversors.put(Boolean.class, booleanConversor);
+    }
     
 }
